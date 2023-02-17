60e99256a4a761247babbe3017f03913bdbe5d41
MizanurRahmanShawon <mr630961@gmail.com>
2023-02-17 20:51:18 +0600

    User deposit and set deposit total value

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,28 @@
-console.log('deposit click');
+/*
+    console.log('deposit click');
+step-1 deposit button added
+step-2  get amount from the deposit input field
+2.5 make sure convert string deposit amount to a number type
+step-4 get the previous deposit total
+
+*/
+
+document.getElementById("btn-deposit").addEventListener("click", function () {
+  const depositField = document.getElementById("deposit-field");
+  // step-2
+  const newDepositAmountString = depositField.value;
+  console.log(newDepositAmountString);
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  // console.log(newDepositAmount);
+
+  // step-3:
+  depositField.value = "";
+
+  // step-4
+  const depositTotalElement = document.getElementById("deposit-total");
+  const previousDepositTotalString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+  // step-5
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
+  depositTotalElement.innerText = newDepositTotal;
+});
